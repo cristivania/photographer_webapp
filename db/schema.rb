d697cc8e761cdd4d3425411d58479407bf7a5765
Cristina Rosario <crosariobatres@yahoo.com>
2018-01-18 12:18:11 -0500

    generate contoller/model Gallery

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180113175521) do
+ActiveRecord::Schema.define(version: 20180118171209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "galleries", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
